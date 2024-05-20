4f820cd8959c7a9e93667a3b9d1050a903920c86
Christoffer Lantz <christoffer.lantz@androidbutiken.net>
2024-05-20 20:03:56 +0200

    add git diff split shortcut

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,8 +1,9 @@
 return {
 	"tpope/vim-fugitive",
-	config = function ()
-		vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = "Git fugitive" })
-		vim.keymap.set('n', '<leader>grf', "<cmd>diffget //2<CR>", { desc = "Git conflict diff left" })
-		vim.keymap.set('n', '<leader>grh', "<cmd>diffget //3<CR>", { desc = "Git conflict diff right" })
-	end
+	config = function()
+		vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git fugitive" })
+		vim.keymap.set("n", "<leader>grf", "<cmd>diffget //2<CR>", { desc = "Git conflict diff left" })
+		vim.keymap.set("n", "<leader>grh", "<cmd>diffget //3<CR>", { desc = "Git conflict diff right" })
+		vim.keymap.set("n", "<leader>gdv", "<cmd>Gdiffsplit<CR>", { desc = "Git diff split" })
+	end,
 }
