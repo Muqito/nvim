a3c2755a326019d577e9c9d72e3f5f6daa9c851d
Christoffer Lantz <christoffer.lantz@androidbutiken.net>
2024-06-21 11:46:31 +0200

    fix harpoon quick menu

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,5 +1,5 @@
 return {
-	'ThePrimeagen/harpoon',
+	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
@@ -7,23 +7,37 @@ return {
 
 		harpoon:setup({})
 
-		vim.keymap.set("n", "<leader>ha", function () harpoon:list():append() end, { desc = "Add file to harpoon" })
-		--vim.keymap.set("n", "<tab>", function () harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Toggle quick file menu" })
-		-- Trying out a new window for quick view
-		vim.keymap.set("n", "<tab>", function() toggle_telescope(harpoon:list()) end,
-			{ desc = "Open harpoon window" })
+		vim.keymap.set("n", "<leader>ha", function()
+			harpoon:list():append()
+		end, { desc = "Add file to harpoon" })
 
-
-		vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "File 1 in buffer" })
-		vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "File 2 in buffer" })
-		vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "File 3 in buffer" })
-		vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "File 4 in buffer" })
-		vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, { desc = "File 5 in buffer" })
-		vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end, { desc = "File 6 in buffer" })
-		vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end, { desc = "File 7 in buffer" })
-		vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end, { desc = "File 8 in buffer" })
-		vim.keymap.set("n", "<leader>9", function() harpoon:list():select(9) end, { desc = "File 9 in buffer" })
-
+		vim.keymap.set("n", "<leader>1", function()
+			harpoon:list():select(1)
+		end, { desc = "File 1 in buffer" })
+		vim.keymap.set("n", "<leader>2", function()
+			harpoon:list():select(2)
+		end, { desc = "File 2 in buffer" })
+		vim.keymap.set("n", "<leader>3", function()
+			harpoon:list():select(3)
+		end, { desc = "File 3 in buffer" })
+		vim.keymap.set("n", "<leader>4", function()
+			harpoon:list():select(4)
+		end, { desc = "File 4 in buffer" })
+		vim.keymap.set("n", "<leader>5", function()
+			harpoon:list():select(5)
+		end, { desc = "File 5 in buffer" })
+		vim.keymap.set("n", "<leader>6", function()
+			harpoon:list():select(6)
+		end, { desc = "File 6 in buffer" })
+		vim.keymap.set("n", "<leader>7", function()
+			harpoon:list():select(7)
+		end, { desc = "File 7 in buffer" })
+		vim.keymap.set("n", "<leader>8", function()
+			harpoon:list():select(8)
+		end, { desc = "File 8 in buffer" })
+		vim.keymap.set("n", "<leader>9", function()
+			harpoon:list():select(9)
+		end, { desc = "File 9 in buffer" })
 
 		-- basic telescope configuration
 		local conf = require("telescope.config").values
@@ -33,17 +47,20 @@ return {
 				table.insert(file_paths, item.value)
 			end
 
-			require("telescope.pickers").new({}, {
-				prompt_title = "Harpoon",
-				finder = require("telescope.finders").new_table({
-					results = file_paths,
-				}),
-				previewer = conf.file_previewer({}),
-				sorter = conf.generic_sorter({}),
-			}):find()
+			require("telescope.pickers")
+				.new({}, {
+					prompt_title = "Harpoon",
+					finder = require("telescope.finders").new_table({
+						results = file_paths,
+					}),
+					previewer = conf.file_previewer({}),
+					sorter = conf.generic_sorter({}),
+				})
+				:find()
 		end
 
-		vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
-			{ desc = "Open harpoon window" })
-	end
+		vim.keymap.set("n", "<tab>", function()
+			toggle_telescope(harpoon:list())
+		end, { desc = "Open harpoon window" })
+	end,
 }
